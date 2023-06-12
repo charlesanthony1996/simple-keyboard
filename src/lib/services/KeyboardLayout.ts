a05710712986d9ba051eb9c1914f9a4977401e0a
cc211002 <cc211002@fhstp.ac.at>
2023-06-12 02:22:03 +0200

    reduced keyboard width to 50%

--- src/lib/services/KeyboardLayout.ts
+++ src/lib/services/KeyboardLayout.ts
@@ -4,7 +4,7 @@ export const getDefaultLayout = () => {
       "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
       "{tab} q w e r t y u i o p [ ] \\",
       "{lock} a s d f g h j k l ; ' {enter}",
-      "{shift} z x c v b n m , . / {shift}",
+      "{shift} z x c v a n m , . / {shift}",
       ".com @ {space}",
     ],
     shift: [
